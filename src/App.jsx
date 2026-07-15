e7e74488c1866720496f12e6cb9cb7ddf3eb6dbe
Lay Lay Zan <132317800+llzan@users.noreply.github.com>
2026-07-14 22:59:48 -0500

    week 2: revisions 2.0

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,10 @@ import TodoList from './TodoList.jsx';
 import TodoForm from './TodoForm.jsx';
 
 function App() {
-
+  
   return (
     <div>
-      <h1>My Todo List</h1>
+      <h1>Todo List</h1>
       <TodoForm />
       <TodoList />
     </div>
--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -7,6 +7,7 @@ function TodoList() {
     {id: 2, title: "take notes"},
     {id: 3, title: "code out app"},
     ]
+    
     return (
        <ul>
         {todoList.map(todo => <li key={todo.id}>{todo.title}</li>)}
