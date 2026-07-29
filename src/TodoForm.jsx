34c35fd25b8877c2a6a28f4c47306a0789794f75
Lay Lay Zan <132317800+llzan@users.noreply.github.com>
2026-07-28 21:53:47 -0500

    lesson-04 manage todos using state

--- src/TodoForm.jsx
+++ src/TodoForm.jsx
@@ -1,16 +1,43 @@
 // TodoForm.jsx (component)
 
+import { useRef } from 'react';
 
-function TodoForm() {
+
+
+
+function TodoForm({ onAddTodo }) {
+    
+    const inputRef = useRef();
+
+    const handleAddTodo = (event) => {
+        even.preventDefault();
+    
+        const todoTitle = event.target.todoTitle.value.trim();
+        if (todoTitle) {
+            onAddTodo(todoTitle);
+            event.target.reset();
+            inputRef.current.focus();
+    }
+};
+    
+    
     return (
-        <form>
-            <label htmlFor="todoTitle">Todo</label>
-            <input type="text" id="todoTitle" />
-            <button type="submit" disabled>Add Todo</button>
-        </form>
+       <form onSubmit={handleAddTodo}>
+        <label htmlFor="todoTitle">Todo</label>
+        <input
+            ref={inputRef}
+            type="text"
+            id="todoTitle"
+            name="todoTitle"
+            placeholder={'Todo text'}
+            required
+        />
+        <button type="submit">
+            Add Todo
+        </button>
+       </form>
 
     );
 }
 
 export default TodoForm;
-
